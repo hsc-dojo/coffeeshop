fba5f9a6e24f6d1cf89968bd42e3f5e9a99797a1
Tj-jm <59123779+Tj-jm@users.noreply.github.com>
2020-10-20 08:44:36 +0600

    failed to connect

--- src/components/Home/info.js
+++ src/components/Home/info.js
@@ -10,7 +10,7 @@ export default function info() {
                 <div className="row">
                     <div className="col-10 col-sm-8 mx-auto text-center">
                         <p className="lead text-muted mb-5">
-                            n the industry's standard dummy text ever since the 1500s, 
+                            n the industry's standard dummy text ever since the 1500s, Lorem
                             when an unknown printer took a galley of type and scrambled it to make a type 
                             specimen book. It has survived not only five centuries, but also the leap into e
                             lectronic typesetting, remaining essentially unchanged. It was popularised in 
