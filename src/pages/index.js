233089d070cb063a781452a6cde969c347cb6fbc
Tj-jm <59123779+Tj-jm@users.noreply.github.com>
2020-10-20 13:57:02 +0600

    svg excluded

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export  const query = graphql`
   img: file(relativePath: {eq: "default-background.jpeg"}) {
     childImageSharp {
       fluid {
-        ...GatsbyImageSharpFluid_tracedSVG
+        ...GatsbyImageSharpFluid
       }
     }
   },
@@ -42,7 +42,7 @@ export  const query = graphql`
         category
         image {
           fixed(width:50,height:50){
-            ...GatsbyContentfulFixed_tracedSVG
+            ...GatsbyContentfulFixed
           }
         }
         
